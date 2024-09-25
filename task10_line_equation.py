fe09d8b2b0bcd4b45a310682236f8bb852ad93ad
Алелеков Ярослав Витальевич <y.alelekov@main.nsu.ru>
2024-09-25 14:00:39 +0700

    сделал 2 задачи

--- task10_line_equation.py
+++ task10_line_equation.py
@@ -3,3 +3,20 @@
 # Пример:
 # Ввод: k = 2, b = 3, x = 5
 # Вывод: y = 13
+'''nums_input= input('')
+nums = nums_input.split(', ')
+for i in range(len(nums)):
+
+print(len(nums))
+
+'''
+'''
+nums_input = input('>>> ')
+nums = nums_input.split()
+index_int = []
+for i in range(len(nums)):
+    if type(nums[i]) == "<class 'int'>":
+        index_int.append(nums[i])
+print(index_int)
+print(type(1))
+'''
